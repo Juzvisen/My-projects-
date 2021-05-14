ec8f77f35c184c759729f5f1b21c56ccd42d554f
Juzvisen <69360152+Juzvisen@users.noreply.github.com>
2021-05-14 14:12:40 +0200

    Add files via upload

---
+++ Madlibs/README.md
@@ -0,0 +1,20 @@
+## Madlibs
+
+<p align="center"><img src="madlibs.png"></p>
+
+Madlibs word substitution game for console / command prompt
+
+* Gives Recomendations for password storage software
+
+## Built With
+
+* [Python](https://www.python.org/) - Backend framework used
+
+## Author
+
+* _Žarko Juzvišen_ - Initial work - [Github](https://github.com/Juzvisen)
+
+## Acknowledgments
+
+* Kylie Ying - Coding Course - [Youtube](https://www.youtube.com/watch?v=8ext9G7xspg)
+* [Redkid.net](http://www.redkid.net/cgi-bin/madlibs/eatdrinkandbesick.pl)
---
+++ Madlibs/madlibs.py
@@ -0,0 +1,31 @@
+print("Eat, drink and be sick") 
+adj1 = input("Adjective1: ")
+adj2 = input("Adjective2: ")
+adv = input ("Adverb: ")
+loa = input("Letter of the Alphabet: ")
+noun1 = input("Noun1: ")
+noun2 = input("Noun2: ")
+noun3 = input("Noun3: ")
+noun4 = input("Noun4: ")
+pob = input("Part of the body: ")
+plur1 = input("Plural noun1: ")
+plur2 = input("Plural noun2: ")
+plur3 = input("Plural noun3: ")
+plur4 = input("Plural noun4: ")
+plur5 = input("Plural noun5: ")
+plur6 = input("Plural noun6: ")
+
+mad_libs = f"An inspector from the Department of Health and {noun1} Services \
+paid a surprise visit to our {adj1} school cafeteria. The lunch \
+special, prepared by our {adj2} dietician, was spaghetti and \
+{noun2}-balls with a choice of either an {noun3} salad or \
+French {plur1}. The inspector found the meat-{plur2} \
+to be overcooked and discovered a live {noun4} in the fries,\
+causing him to have a {pob} ache. In response, he \
+threw up all over his {plur3}. In his report, the inspector \
+{adv} recommended that the school cafeteria serve only \
+nutritious {plur4} as well as low-calorie {plur5}, and \
+that all of the saturated {plur6} be eliminated. He rated the \
+cafeteria a {loa}-minus."
+
+print(mad_libs)
